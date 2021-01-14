877831b1318b225d8c13a49033893bb7dd1e2852
Ilya Malov <IlyamalovCES@gmail.com>
2021-01-15 00:37:46 +0300

    init

---
+++ model.py
@@ -0,0 +1,18 @@
+# import dvc.api
+import joblib
+import os
+from subprocess import check_output
+
+class Model:
+  def __init__(self):
+    # with dvc.api.open('models/m.model', mode='rb') as f:
+    #   self.model = joblib.load(f)
+    with open('model/m.model', mode='rb') as f:
+      self.model = joblib.load(f)
+
+  def predict(self, X):
+    if self.model is None:
+     raise Exception('Model not trained yet')
+
+    prediction = self.model.predict([X])
+    return prediction[0]
